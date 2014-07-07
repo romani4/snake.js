185a9a9594d99e73d3f6267e6f70e5531395c1bb
romanov <dima.romanov.1990@gmail.com>
2014-07-07 18:44:30 +0700

    app.js

---
+++ src/app.js
@@ -0,0 +1,26 @@
+/**
+ * @author romani4 <dima.romanov.1990@gmail.com>
+ */
+
+var App = (function() {
+    'use strict';
+
+    var defaultSettings = {
+        cycleDuration: 1000,
+        snakeDefaultSize: 10,
+        snakeIncrementalSize: 5
+    };
+
+    function App(settings) {
+        this.settings = defaultSettings; // TODO: extend
+        console.log('run');
+    }
+
+    return App;
+})();
+
+(function(){
+    this.onload = function(){
+        this.App = new App( {} );
+    }
+}).call(window);
