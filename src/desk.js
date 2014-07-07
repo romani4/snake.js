3bb74a1a2b50cc6c33533bff71b065a3d3385190
romanov <dima.romanov.1990@gmail.com>
2014-07-07 20:20:07 +0700

    Desk basic operations

--- src/desk.js
+++ src/desk.js
@@ -7,7 +7,25 @@ var Desk =(function() {
     'use strict';
 
     function Desk(canvasElement, xSize, ySize, unitSize) {
-        console.log('Desk not implemented yet'); // TODO: implement
+        this.canvasElement = canvasElement;
+        this.xSize = xSize;
+        this.ySize = ySize;
+        this.unitSize = unitSize;
+
+        this.canvasElement.setAttribute('width', xSize*unitSize);
+        this.canvasElement.setAttribute('height', ySize*unitSize);
+        this.drawingArea = this.canvasElement.getContext("2d");
     }
+
+    Desk.prototype.clean = function(color){
+        this.drawingArea.fillStyle = color;
+        this.drawingArea.fillRect( 0, 0, this.xSize*this.unitSize, this.ySize*this.unitSize);
+    };
+
+    Desk.prototype.putField = function(x,y,color){
+        this.drawingArea.fillStyle = color;
+        this.drawingArea.fillRect( x*this.unitSize + 1 , y*this.unitSize+1, this.unitSize-1 , this.unitSize-1 );
+    };
+
     return Desk;
 })();
